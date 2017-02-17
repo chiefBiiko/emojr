08cbd51488a451416ed62ff9dd082f44b12905a1
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-02-17 14:34:56 +0100

    first few faces

---
+++ faces.R
@@ -0,0 +1,27 @@
+# emojr
+#
+# Usage
+#   source('https://github.com/chiefBiiko/emojr/raw/master/faces.R')
+#   cat(AA$bird, AA$cat, sep='\n')  # pretty-print some
+#   cat(unlist(AA), sep='\n')  # pretty-print all
+
+AA <- list(
+  yo    = '(-*.*)',
+  stun  = '(-*,*)',
+  poker = '(-*_*)',
+  down  = '(-+_+)',
+  k.o.  = '(-x_x)',
+  nerd  = '(-p_p)',
+  cream = '(-$_$)',
+  high  = '(-=_=)',
+  lean  = '(-~_~)',
+  hmpf  = '(-*~*)',
+  nose  = '(-*u*)',
+  kiss  = '(-*3*)',
+  bla   = '(-*c*)::',
+  shout = '(-*o*):::',
+  cat   = '=(-*x*)=',
+  bird  = '<(-*v*)>',
+  teddy = '°(-*Y*)°',
+  snake = '__n_n_(-°w°)'
+)
